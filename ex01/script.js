5ccb2308bab2654b17f8496619598ed5e395c852
Vinicius de Andrade <viniciusy62@gmail.com>
2022-07-19 23:45:12 -0300

    upgrade

--- ex01/script.js
+++ ex01/script.js
@@ -6,16 +6,16 @@ function horaDia() {
     mudarHora.innerHTML = `Agora são ${hora} horas`
     
     if(hora > 0 && hora < 12) {
-        document.body.style.background = "yellow"
+        document.body.style.background = "#e8f076"
         mudarImagem.src = "amanhecer01.jpg"
     } else if (hora >= 12 && hora <= 15) {
-        document.body.style.background = "red"
+        document.body.style.background = "#f0bf76"
         mudarImagem.src = "meiodia.jpg"
     } else if (hora >= 16 && hora <= 18) {
-        document.body.style.background = "green"
+        document.body.style.background = "#f08e76"
         mudarImagem.src = "entardecer.jpeg"
     } else {
-        document.body.style.background = "black"
+        document.body.style.background = "#1a0f0d"
         mudarImagem.src = "noite.jpg"
     }
 }
